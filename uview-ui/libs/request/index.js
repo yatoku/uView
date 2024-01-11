a78eb87e03a3da29d4baa016a512ecf7904b1acb
luminous <35695074+yatoku@users.noreply.github.com>
2024-01-11 15:40:20 +0800

    Update index.js

--- uview-ui/libs/request/index.js
+++ uview-ui/libs/request/index.js
@@ -15,7 +15,7 @@ class Request {
 			let interceptorRequest = this.interceptor.request(options);
 			if (interceptorRequest === false) {
 				// 返回一个处于pending状态中的Promise，来取消原promise，避免进入then()回调
-				return new Promise(()=>{});
+				return new Promise(() => {});
 			}
 			this.options = interceptorRequest;
 		}
@@ -23,7 +23,7 @@ class Request {
 		options.responseType = options.responseType || this.config.responseType;
 		options.url = options.url || '';
 		options.params = options.params || {};
-		options.header = Object.assign({}, this.config.header, options.header);
+		options.header = Object.assign(this.config.header, options.header);
 		options.method = options.method || this.config.method;
 
 		return new Promise((resolve, reject) => {
@@ -34,10 +34,10 @@ class Request {
 				clearTimeout(this.config.timer);
 				this.config.timer = null;
 				// 判断用户对拦截返回数据的要求，如果originalData为true，返回所有的数据(response)到拦截器，否则只返回response.data
-				if(this.config.originalData) {
+				if (this.config.originalData) {
 					// 判断是否存在拦截器
 					if (this.interceptor.response && typeof this.interceptor.response === 'function') {
-						let resInterceptors = this.interceptor.response(response);
+						let resInterceptors = this.interceptor.response(response,options,this.config);
 						// 如果拦截器不返回false，就将拦截器返回的内容给this.$u.post的then回调
 						if (resInterceptors !== false) {
 							resolve(resInterceptors);
@@ -51,7 +51,8 @@ class Request {
 					}
 				} else {
 					if (response.statusCode == 200) {
-						if (this.interceptor.response && typeof this.interceptor.response === 'function') {
+						if (this.interceptor.response && typeof this.interceptor.response ===
+							'function') {
 							let resInterceptors = this.interceptor.response(response.data);
 							if (resInterceptors !== false) {
 								resolve(resInterceptors);
@@ -75,13 +76,15 @@ class Request {
 			}
 
 			// 判断用户传递的URL是否/开头,如果不是,加上/，这里使用了uView的test.js验证库的url()方法
-			options.url = validate.url(options.url) ? options.url : (this.config.baseUrl + (options.url.indexOf('/') == 0 ?
-				options.url : '/' + options.url));
-			
+			// options.url = validate.url(options.url) ? options.url : (this.config.baseUrl + (options.url.indexOf('/') == 0 ?
+			// 	options.url : '/' + options.url));
+
+			options.url = validate.url(options.url) ? options.url : (this.config.baseUrl + options.url);
+
 			// 是否显示loading
 			// 加一个是否已有timer定时器的判断，否则有两个同时请求的时候，后者会清除前者的定时器id
 			// 而没有清除前者的定时器，导致前者超时，一直显示loading
-			if(this.config.showLoading && !this.config.timer) {
+			if (this.config.showLoading && !this.config.timer) {
 				this.config.timer = setTimeout(() => {
 					uni.showLoading({
 						title: this.config.loadingText,
@@ -104,6 +107,7 @@ class Request {
 			baseUrl: '', // 请求的根域名
 			// 默认的请求头
 			header: {},
+			is_login:'',
 			method: 'POST',
 			// 设置为json，返回后uni.request会对数据进行一次JSON.parse
 			dataType: 'json',
@@ -116,7 +120,7 @@ class Request {
 			originalData: false, // 是否在拦截器中返回服务端的原始数据，见文档说明
 			loadingMask: true, // 展示loading的时候，是否给一个透明的蒙层，防止触摸穿透
 		}
-	
+
 		// 拦截器
 		this.interceptor = {
 			// 请求前的拦截
@@ -144,7 +148,7 @@ class Request {
 				data
 			})
 		}
-		
+
 		// put请求，不支持支付宝小程序(HX2.6.15)
 		this.put = (url, data = {}, header = {}) => {
 			return this.request({
@@ -154,7 +158,7 @@ class Request {
 				data
 			})
 		}
-		
+
 		// delete请求，不支持支付宝和头条小程序(HX2.6.15)
 		this.delete = (url, data = {}, header = {}) => {
 			return this.request({
